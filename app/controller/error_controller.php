490a02fbd7921c4b8cf99b44cefe56c7d6c5ed32
Henry Shum <henry@metaseit.com>
2016-11-01 12:38:00 +0800

    fix typo in error controller

--- app/controller/error_controller.php
+++ app/controller/error_controller.php
@@ -2,7 +2,7 @@
 <fusedoc>
 	<io>
 		<in>
-			<string name="$boxy->error" />
+			<string name="$fusebox->error" />
 		</in>
 		<out />
 	</io>
@@ -12,15 +12,15 @@
 // just show textual message (when ajax request)
 // ===> rely on javascript to style the message
 if ( F::ajaxRequest() ) {
-	die( $boxy->error );
+	die( $fusebox->error );
 
 // show error message with global layout (when normal request)
 } else {
 	// define flash type
-	if ( $boxy->error == 'page not found' ) {
-		$arguments['flash'] = array('type' => 'warning', 'message' => "<i class='fa fa-exclamation-circle'></i> <strong>".F::fuseaction()."</strong> - {$boxy->error}");
+	if ( $fusebox->error == 'page not found' ) {
+		$arguments['flash'] = array('type' => 'warning', 'message' => "<i class='fa fa-exclamation-circle'></i> <strong>".F::fuseaction()."</strong> - {$fusebox->error}");
 	} else {
-		$arguments['flash'] = array('type' => 'danger', 'message' => "<i class='fa fa-exclamation-circle'></i> <strong>".F::fuseaction()."</strong> - {$boxy->error}");
+		$arguments['flash'] = array('type' => 'danger', 'message' => "<i class='fa fa-exclamation-circle'></i> <strong>".F::fuseaction()."</strong> - {$fusebox->error}");
 	}
 	// layout
 	include F::config('appPath').'view/global/layout.php';
