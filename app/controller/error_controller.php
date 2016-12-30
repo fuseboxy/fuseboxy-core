14b71a6f943e9a56566a7067ed75c3d4000e0fdb
Henry Shum <henry@metaseit.com>
2016-12-30 15:05:59 +0800

    apply simpler error-controller for the core

--- app/controller/error_controller.php
+++ app/controller/error_controller.php
@@ -8,20 +8,4 @@
 	</io>
 </fusedoc>
 */
-
-// just show textual message (when ajax request)
-// ===> rely on javascript to style the message
-if ( F::ajaxRequest() ) {
-	die( $fusebox->error );
-
-// show error message with global layout (when normal request)
-} else {
-	// define flash type
-	if ( $fusebox->error == 'page not found' ) {
-		$arguments['flash'] = array('type' => 'warning', 'message' => "<i class='fa fa-exclamation-circle'></i> <strong>".F::command()."</strong> - {$fusebox->error}");
-	} else {
-		$arguments['flash'] = array('type' => 'danger', 'message' => "<i class='fa fa-exclamation-circle'></i> <strong>".F::command()."</strong> - {$fusebox->error}");
-	}
-	// layout
-	include F::config('appPath').'view/global/layout.php';
-}
+die($fusebox->error);
