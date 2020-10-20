91295a1723ecf8873ad3be0374a238d54896d998
henrygotmojo <henrygotmojo@gmail.com>
2020-10-20 12:42:53 +0800

    include framework core from vendor path or app path

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+$GLOBALS['startTick'] = microtime(true) * 1000;
 // debug settings
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
@@ -9,6 +10,12 @@ header("X-UA-Compatible: IE=Edge");
 // session management
 session_name('FUSEBOXY');
 session_start();
-// load framework and run!
-include 'app/framework/fuseboxy.php';
+// load env settings (when necessary)
+if ( is_file('_env.php') ) include '_env.php';
+// load framework
+if ( is_file(__DIR__.'/vendor/fuseboxy/fuseboxy-core/app/framework/fuseboxy.php') ) {
+	include __DIR__.'/vendor/fuseboxy/fuseboxy-core/app/framework/fuseboxy.php';
+	Framework::$configPath = __DIR__.'/app/config/fusebox_config.php';
+} else include 'app/framework/fuseboxy.php';
+// run!!
 Framework::run();
