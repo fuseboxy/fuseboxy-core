69d4a2e539780baa3dfdeb99696cba2e5fd10069
henrygotmojo <henrygotmojo@gmail.com>
2020-11-17 15:55:37 +0800

    set default timezone to hong kong

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-$GLOBALS['startTick'] = microtime(true) * 1000;
+$GLOBALS['startTick'] = microtime(true)*1000;
 // debug settings
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 // environment settings
-date_default_timezone_set('Asia/Taipei');
+date_default_timezone_set('Asia/Hong_Kong');
 // for disable ie-compatible mode
 header("X-UA-Compatible: IE=Edge");
 // session management
